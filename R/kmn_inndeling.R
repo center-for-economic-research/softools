fa7e6b63e13d4bbb7b8fc2bec1b8ba637faf82f3
Mads <mads.wold@samforsk.no>
2023-03-23 15:03:35 +0100

    Municipality classification function takes year as argument

--- R/kmn_inndeling.R
+++ R/kmn_inndeling.R
@@ -1,32 +1,33 @@
 #' Get municipality classifications from SSB
 #'
-#' @param datoer
-#' String for the required date of the classification. Format must be "yyyy-mm-dd". For an inverval, provide two dates as a vector. If blank, will default to today's date
-#'
+#' @param aar Integer vector with the years for which you want valid classifications.
 #'
 #' @return A data frame with municipality name, number and year of validity
 #' @export
 #'
 #' @examples
-#' kommunestruktur_2020 <- kmn_inndeling(datoer = "2020-01-01")
+#' kommunestruktur_2020 <- kmn_inndeling(aar = 2020)
 #' head(kommunestruktur_2020)
 #'
 #' @importFrom rlang .data
 #'
-kmn_inndeling <- function(datoer) {
+kmn_inndeling <- function(aar) {
 
-  names(datoer) <- datoer
+  datoer <- paste0(aar, "-01-01")
+
+  names(datoer) <- aar
+
   kmn_inndeling <-
     purrr::map_dfr(
     datoer,
     ~ klassR::GetKlass(131, date = .x),
-    .id = "dato"
+    .id = "aar"
   ) |>
     dplyr::rename(
       "knavn" = .data$name,
       "knr" = .data$code
     ) |>
-    dplyr::select(.data$knavn, .data$knr, .data$dato)
+    dplyr::select(.data$knavn, .data$knr, .data$aar)
 
   return(kmn_inndeling)
 
--- tests/testthat/test-kmn_inndeling.R
+++ tests/testthat/test-kmn_inndeling.R
@@ -1,6 +1,6 @@
 test_that("Municipality structure is output as a data frame", {
   expect_s3_class(
-    kmn_inndeling("2020-01-01"),
+    kmn_inndeling(2020),
     "data.frame"
   )
 }
